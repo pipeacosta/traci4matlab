508b1a6285305f4980a79ec7fb5156b9d2e983ef
afacostag <afacostag@99561c03-bbc3-48a5-b567-19c10ecd6112>
2014-03-04 12:02:31 +0000

    A correction related to the license.

--- trunk/TraCI4Matlab/+traci/constants.m
+++ trunk/TraCI4Matlab/+traci/constants.m
@@ -5,12 +5,6 @@ classdef constants
     %   Politecnico Jaime Isaza Cadavid.
     %   Authors: Andres Acosta, Jairo Espinosa, Jorge Espinosa.
 	%   $Id$
-	%
-	%   This file is part of TraCI4Matlab.
-	%   TraCI4Matlab is free software: you can redistribute it and/or modify
-	%   it under the terms of the GNU General Public License as published by
-	%   the Free Software Foundation, either version 3 of the License, or
-	%   (at your option) any later version.
 
 	properties (Constant = true)
 		% ****************************************
